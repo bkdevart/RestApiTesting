34a10ed1eead63e3265794f102dbc42ac2541601
Brandon Knox <bknox.dev@gmail.com>
2021-04-20 16:12:04 -0700

    Initial Commit

---
+++ RestApi/RestApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestApiApp.swift
+//  RestApi
+//
+//  Created by Brandon Knox on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct RestApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
