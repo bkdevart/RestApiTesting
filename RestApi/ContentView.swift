917ef52d65dc1f0af5f994feb0c04a3182a8b7d5
Brandon Knox <bknox.dev@gmail.com>
2021-04-21 16:44:51 -0700

    Set up initial implementation - does not work with data yet

--- RestApi/ContentView.swift
+++ RestApi/ContentView.swift
@@ -7,10 +7,63 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var id: Int
+    var title: String
+    var code: String
+    var linenos: Bool
+    var language: String  // = "Python"
+    var style: String
+}
+
 struct ContentView: View {
+    @State var results = [Result]()
+//    @ObservedObject var query = Query()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Button("Search") {
+                    loadData()
+                    print("Searching...")
+                }
+                List(results, id: \.id) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.title)
+                            .font(.headline)
+                    }
+                }
+            }
+        }
+    }
+    
+    func loadData() {
+        // http://127.0.0.1:8000/snippets/
+        // http://127.0.0.1:8000/snippets/2/
+        guard let url = URL(string: "http://127.0.0.1:8000/snippets/") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try?JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    print("Results returned")
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            print(request)
+        }.resume()
     }
 }
 
